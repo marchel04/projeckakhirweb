a103121b90623bda9cc6eab5ba843abc5e6ce162
marchel04 <144196935+marchel04@users.noreply.github.com>
2025-06-26 14:50:15 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const cacheName = 'portfolio-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/project.html',
+  '/style.css',
+  '/manifest.json',
+  '/images/saya.jpg',
+  '/images/aws.png',
+  '/images/Pemrograman_Komputer.png',
+  '/images/html.jpg',
+  '/images/css.jpg',
+  '/images/java.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(assets);
+      })
+      .catch(error => {
+        console.error('Gagal cache:', error);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
